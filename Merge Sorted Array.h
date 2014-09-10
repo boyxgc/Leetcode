3a01ba3ca1c9bc78429c909dcb2b7f05985942f2
boyxgc <boyxgc@gmail.com>
2014-09-09 19:49:03 -0700

    update solutionf or Merge sorted Array

--- Merge Sorted Array.h
+++ Merge Sorted Array.h
@@ -1,25 +1,17 @@
 class Solution {
 public:
     void merge(int A[], int m, int B[], int n) {
-        vector<int> A2(m, 0);
-        for(int i = 0; i < m; ++i) 
-            A2[i] = A[i];
-            
-        int pos = 0;
-        int pos1 = 0;
-        int pos2 = 0;
+        int end = m + n -1;
+        int a = m - 1;
+        int b = n - 1;
         
-        while(pos1 < m && pos2 < n) {
-            if(A2[pos1] < B[pos2])
-                A[pos++] = A2[pos1++];
-            else
-                A[pos++] = B[pos2++];
-        } 
-        while(pos1 < m) {
-            A[pos++] = A2[pos1++];
+        while(b >= 0) {
+            if(a >= 0 && A[a] > B[b]) {
+                A[end--] = A[a--];
+            } else {
+                A[end--] = B[b--];
+            }
         }
-        while(pos2 < n) {
-            A[pos++] = B[pos2++];
-        }
+        return;
     }
 };
