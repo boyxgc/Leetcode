f7bf27d8d532bcf6b762d328f360460243f2b8f2
boyxgc <boyxgc@gmail.com>
2014-09-09 23:37:36 -0700

    update solution to Minimum Path Sum

--- Minimum Path Sum.h
+++ Minimum Path Sum.h
@@ -1,30 +1,23 @@
 class Solution {
 public:
     int minPathSum(vector<vector<int> > &grid) {
-        int n = grid.size();
-        int m = grid[0].size();
         
-        vector<vector<int> > opt;
-        for(int i = 0; i <= n; ++i){// 1 more row and colummn
-            vector<int> tmpv(m+1,-1);
-            opt.push_back(tmpv);
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        vector<int> uprow;
+        uprow.push_back(grid[0][0]);
+        for(int i = 1; i < n; ++i) {
+            uprow.push_back(uprow[i-1] + grid[0][i]);
         }
         
-        for(int i = 1; i <= n; ++i){// starts from row 1
-            for(int j = 1; j <= m; ++j) {
-                int minsofar = -1;
-                opt[i][j] = grid[i-1][j-1] + max(0, mymin(minsofar,mymin(opt[i-1][j], opt[i][j-1])));
+        for(int row = 1; row < m; ++row) {
+            uprow[0] = uprow[0] + grid[row][0];
+            for(int col = 1; col < n; ++col) {
+                uprow[col] = min(uprow[col - 1], uprow[col]) + grid[row][col];
             }
         }
         
-        return opt[n][m];
-    }
-    
-    int mymin(int minsofar, int num) {
-        if(minsofar < 0)
-            return num;
-        if(num < 0)
-            return minsofar;
-        return min(minsofar, num);
+        return uprow[n-1];
     }
 };
