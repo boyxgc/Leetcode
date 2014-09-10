3d65982dcaf396b0637fad89eb02b7746379e5ae
boyxgc <boyxgc@gmail.com>
2014-09-09 21:12:46 -0700

    update solution for Unique paths

--- Unique Paths.h
+++ Unique Paths.h
@@ -1,19 +1,12 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int> >opt;
-        for(int i = 0; i <=m; ++i){
-            vector<int> tmpv(n+1, 0);
-            opt.push_back(tmpv);
-        }
-        opt[1][1] = 1;
-        for(int i = 1; i <= m; ++i) {
-            for(int j = 1; j <= n; ++j) {
-                if(i != 1 || j != 1)//skip opt[1][1]
-                    opt[i][j] = opt[i-1][j] + opt[i][j-1];
+        vector<int> row(n, 1);// only store the last row
+        for(int i = 1; i < m; ++i) {
+            for(int j = 1; j < n; ++j) {
+                row[j] = row[j] + row[j - 1]; // up + left
             }
         }
-        
-        return opt[m][n];
+        return row[n-1];
     }
 };
