97de335ba182192e0c69017c389c31cea4d963d7
boyxgc <boyxgc@gmail.com>
2014-09-11 16:37:37 -0700

    update solution for Letter Combinations of a Phone Number

--- Letter Combinations of a Phone Number.h
+++ Letter Combinations of a Phone Number.h
@@ -1,45 +1,42 @@
 class Solution {
 public:
-    string digit2string(int i) {
-        switch(i) {
-            case 0: return " ";
-            case 2: return "abc";
-            case 3: return "def";
-            case 4: return "ghi";
-            case 5: return "jkl";
-            case 6: return "mno";
-            case 7: return "pqrs";
-            case 8: return "tuv";
-            case 9: return "wxyz";
-            default: return "%";
-        }
-    }
     vector<string> letterCombinations(string digits) {
+        string empty;
         vector<string> res;
         
-        if(digits == ""){
-            res.push_back("");
-            return res;
-        }
-            
-        for(int i = 0; i < digits.size(); ++i) {
-            string letters = digit2string(digits[i]-'0');
-            if(res.size() == 0) {
-                for(int p = 0; p < letters.length(); ++p)
-                    res.push_back(string(1, letters[p]));
-            } else {
-                int currsize = res.size();
-                for(int j = 0; j < currsize; ++j) {
-                    string str = res[j];
-                    res[j] = str+letters[0];
-                    if(letters.size() > 1) {
-                        for(int p = 1; p < letters.length(); ++p)
-                            res.push_back(str+letters[p]);
-                    }
-                }
-            }
-        }
+        generateCombinations(digits, 0, empty, res);
         
         return res;
+    }
+    
+private:
+    void generateCombinations(string &digits, int pos, string &curr, vector<string> &res) {
+        if(pos == digits.size()) {
+            string copy = curr;
+            res.push_back(copy);
+            return;
+        }
+        
+        string chars = decode(digits[pos]);
+        
+        for(int i = 0; i < chars.size(); ++i) {
+            curr.push_back(chars[i]);
+            generateCombinations(digits, pos + 1, curr, res);
+            curr.pop_back();
+        }
+    }
+
+    string decode(char c) {
+        switch(c) {
+            case '2' : return "abc";
+            case '3' : return "def";
+            case '4' : return "ghi";
+            case '5' : return "jkl";
+            case '6' : return "mno";
+            case '7' : return "pqrs";
+            case '8' : return "tuv";
+            case '9' : return "wxyz";
+            default : return "";
+        }
     }
 };
