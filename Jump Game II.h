89241fcc3087d0387350065a1d058bc6f9d71e7a
boyxgc <boyxgc@gmail.com>
2014-09-12 16:27:17 -0700

    update solution for Jump Gamge II

--- Jump Game II.h
+++ Jump Game II.h
@@ -1,17 +1,23 @@
 class Solution {
 public:
     int jump(int A[], int n) {
-        vector<int> steps(n, n);
+        
+        vector<int> steps(n, INT_MAX);
         steps[0] = 0;
-        int curr_covered = 0;
-        for(int i = 0; i < n; ++i){
-            if(i+A[i] <= curr_covered)
-                continue;
-            for(int j = curr_covered+1; j <= A[i]+i && j < n; ++j){
-                steps[j] = min(steps[j], steps[i]+1);
+        
+        int farthest = 0;
+        for(int i = 0; i < n; ++i) {
+            int could_cover = i + A[i];
+            if(could_cover > farthest) {
+                for(int j = farthest+1; j <= could_cover && j < n; j++) {
+                    steps[j] = steps[i] + 1;
+                }
+                farthest = could_cover;
             }
-            curr_covered = A[i]+i;
+            
+            if(farthest == n-1) break;
         }
+        
         return steps[n-1];
     }
 };
