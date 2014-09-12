affc7b0202bac1dce36aae1336c416c6a1d3d4f4
boyxgc <boyxgc@gmail.com>
2014-09-11 17:33:49 -0700

    update solution for Palindrome Partitioning

--- Palindrome Partitioning.h
+++ Palindrome Partitioning.h
@@ -1,31 +1,50 @@
 class Solution {
 public:
-    bool isPal(string s){
-        string s2 = s;
-        reverse(s2.begin(), s2.end());
-        return s == s2;
-    }
     vector<vector<string>> partition(string s) {
-        vector<vector<string> > ret;
-        if(s==""){
-            return ret;
+        vector<vector<string> > res;
+        if(s.empty()) return res;
+        
+        vector<bool> v(s.size(), false);
+        vector<vector<bool> > isPal(s.size(), v);
+        
+        preprocess(isPal, s); //O(n^2)
+        
+        vector<string> empty;
+        generateParts(s, 0, empty, res, isPal); //O(n^2)
+        
+        return res;
+    }
+    
+private:
+    void generateParts(const string &s, int pos, vector<string> & curr, vector<vector<string> > & res, const vector<vector<bool> > &isPal) {
+        if(pos == s.size()) {
+            vector<string> copy(curr);
+            res.push_back(curr);
+            return;
         }
-        if(isPal(s)){
-            vector<string> tmp;
-            tmp.push_back(s);
-            ret.push_back(tmp);
-        }
-            
-        for(int k = 1; k < s.size(); ++k) {
-            vector<vector<string> >right;
-            if (!isPal(s.substr(0, k))) 
-                continue;
-            right = partition(s.substr(k));
-            for(int i = 0; i < right.size(); ++i){
-                right[i].insert(right[i].begin(), s.substr(0, k));
-                ret.push_back(right[i]);
+        
+        for(int i = pos; i < s.size(); ++i) {
+            if(isPal[pos][i]) {
+                curr.push_back(s.substr(pos, i - pos + 1));
+                generateParts(s, i+1, curr, res, isPal);
+                curr.pop_back();
             }
         }
-        return ret;
+    }
+
+    void preprocess(vector<vector<bool> > &isPal, const string &s) {
+        for(int i = 0; i < s.size(); ++i) {
+            isPal[i][i] = true;
+        }
+        for(int len = 2; len <= isPal.size(); ++len) {
+            for(int i = 0; i < isPal.size() - len + 1; ++i) {
+                int a = i+len - 1;
+                if(len < 3) {
+                    isPal[i][a] = (s[i] == s[a]);
+                } else {
+                    isPal[i][a] = (s[i] == s[a]) && isPal[i+1][a-1];
+                }
+            }
+        }
     }
 };
