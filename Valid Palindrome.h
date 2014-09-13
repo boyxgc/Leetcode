c743041e991e3a0fa9325f791d0827e7b0993442
boyxgc <boyxgc@gmail.com>
2014-09-12 18:52:42 -0700

    update solution for Valid Palindrome

--- Valid Palindrome.h
+++ Valid Palindrome.h
@@ -1,31 +1,26 @@
 class Solution {
 public:
-    char toCap(char c){
-        if(c >= 'a' && c <= 'z')
-            return c+'A'-'a';
-        return c;
-    }
-    char isAlphaNum(char c){
-        return (c >= 'a' && c <= 'z')||(c >= 'A' && c <= 'Z')||(c >='0' && c<='9');
-    }
     bool isPalindrome(string s) {
-        int n = s.size();
-        int l=0;
-        int r=n-1;
-        while(l <= r){
-            if(!isAlphaNum(s[l])){
+        int l = 0;
+        int h = s.size() - 1;
+        while(l < h) {
+            if(!isalpha(s[l]) && !isdigit(s[l])) {
                 l++;
                 continue;
             }
-            if(!isAlphaNum(s[r])){
-                r--;
+            if(!isalpha(s[h]) && !isdigit(s[h])) {
+                h--;
                 continue;
             }
-            if(toCap(s[l]) != toCap(s[r]))
-                return false;
+            if(toLower(s[l]) != toLower(s[h])) return false;
             l++;
-            r--;
+            h--;
         }
         return true;
+    }
+    
+    int toLower(int c) {
+        if(isalpha(c)) return tolower(c);
+        else return c;
     }
 };
