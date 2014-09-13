f76699ae2210bb630d675413666a84a3351c19af
boyxgc <boyxgc@gmail.com>
2014-09-12 23:06:57 -0700

    update solution for Implement strStr

--- Implement strStr().h
+++ Implement strStr().h
@@ -1,35 +1,26 @@
 class Solution {
 public:
     char *strStr(char *haystack, char *needle) {
-        if(strcmp(needle,"") == 0)
-            return haystack;
-        if(strcmp(haystack,"") == 0)
-            return NULL;
-        char *pos_l = haystack;
-        char *pos_r = haystack;
-        char *pos2 = needle;
+        if(!haystack || !needle) return NULL;
+        int lenh = strlen(haystack);
+        int lenn = strlen(needle);
+        if( lenh < lenn) return NULL;
         
-        while(*pos_r && *pos2){
-            pos_r++;
-            pos2++;
-        }
-        if(*pos2)
-            return NULL;
-        pos_r--;
-        while(*pos_r){
-            char *tmppos_l = pos_l;
-            pos2 = needle;
-            while(tmppos_l <= pos_r){
-                if(*tmppos_l != *pos2)
-                    break;
-                tmppos_l++;
-                pos2++;
+        for(int i = 0; i < lenh - lenn +1; ++i) {
+            if(check(haystack+i, needle)) {
+                return haystack+i;
             }
-            if(!(*pos2))
-                return pos_l;
-            pos_l++;
-            pos_r++;
         }
+        
         return NULL;
+    }
+    
+    bool check(char *h, char *n) {
+        while(*h != '\0' && *n != '\0') {
+            if(*h != *n) return false;
+            h++;
+            n++;
+        }
+        return true;
     }
 };
