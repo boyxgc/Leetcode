45dca331a5638704281cd1c264672ddc0b4d261f
boyxgc <boyxgc@gmail.com>
2014-09-12 22:33:55 -0700

    update solution for Permutation Sequence

--- Permutation Sequence.h
+++ Permutation Sequence.h
@@ -1,29 +1,29 @@
 class Solution {
 public:
     string getPermutation(int n, int k) {
-        vector<int> factorial(n+1,1);
-        factorial[0] = 1;
-        for(int i = 1; i <= n; ++i)
-            factorial[i] = i*factorial[i-1];
-        vector<int> digits;
-        for(int i = 1; i <= n; ++i)
-            digits.push_back(i);
-            
-        return getP(n, k-1, digits, factorial);
+        vector<int> factorials(n+1, 1);
+        string str = "";
+        for(int i = 1; i <= n; ++i) {
+            factorials[i] = i*factorials[i-1];
+            str += to_string(i);
+        }
+        return findPermutation(str, k-1, factorials);
     }
     
-    string getP(int n, int k, vector<int> &digits, vector<int> &f){
-        if(k == 0 || digits.size() < 2){
-            string ret ="";
-            for(int i = 0; i < digits.size(); ++i)
-                ret += to_string(digits[i]);
-            return ret;
+    string findPermutation(string str, int kth, vector<int> &fact) {
+        if(kth == 0) {
+            return str;
         }
-        int whichSubTree = k/(f[n-1]);
-        string fixed;
-        fixed = to_string(digits[whichSubTree]);
-        digits.erase(digits.begin()+whichSubTree);
-        return fixed + getP(n-1, k%(f[n-1]), digits, f);
+        
+        int n = str.size();
+        int subtree_size = fact[n-1];
+        
+        int n_subtree = kth/subtree_size;
+        int kth_sub = kth%subtree_size;
+        
+        char c = str[n_subtree];
+        str.erase(str.begin() + n_subtree);
+        return c + findPermutation(str, kth_sub, fact);
     }
-
+    
 };
