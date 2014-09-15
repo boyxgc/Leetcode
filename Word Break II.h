ac044365b9407a103f9347c81105e647d437d774
boyxgc <boyxgc@gmail.com>
2014-09-14 17:25:27 -0700

    update solution for Word Break II

--- Word Break II.h
+++ Word Break II.h
@@ -1,6 +1,40 @@
 class Solution {
 public:
     vector<string> wordBreak(string s, unordered_set<string> &dict) {
+        if(s.size() == 0) return vector<string>();
+        unordered_map<string, vector<string> > cache;
+        return wordBreak(s, cache, dict);
+    }
+    
+    vector<string> wordBreak(string str, unordered_map<string, vector<string> > &cache, unordered_set<string> &dict) {
+        if(str.size() == 0) {
+            vector<string> v;
+            v.push_back("");
+            return v;
+        }
+        
+        if(cache.find(str) != cache.end()) return cache[str];
+        
+        vector<string> res;
+        for(int len = 1; len <= str.size(); ++len) {
+            string word = str.substr(0, len);
+            if(dict.find(word) == dict.end()) continue;
+            
+            vector<string> nextbreak = wordBreak(str.substr(len), cache, dict);
+            for(int i = 0; i < nextbreak.size(); ++i) {
+                string tmp = nextbreak[i] == "" ? word : word+" "+nextbreak[i];
+                res.push_back(tmp);
+            }
+        }
+        
+        cache[str] = res;
+        return res;
+    }
+};
+
+class Solution {
+public:
+    vector<string> wordBreak(string s, unordered_set<string> &dict) {
         vector<vector<bool> >dictMap(s.size(), vector<bool>(s.size(), false));
         for(int i = 0; i < s.size(); ++i){
             for(int j = i; j < s.size(); ++j){
