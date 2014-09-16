0e1913a60aa7db9780eddce17ab2820fd04a91db
boyxgc <boyxgc@gmail.com>
2014-09-15 21:08:45 -0700

    update solution for Word Latter II

--- Word Ladder II.h
+++ Word Ladder II.h
@@ -5,6 +5,95 @@
 #include <vector>
 using namespace std;
 /*
+time limit excceed
+class Solution {
+public:
+    struct Node {
+        string str;
+        vector<string> prevs;
+        Node(string s): str(s) {}
+    };
+    vector<vector<string>> findLadders(string start, string end, unordered_set<string> &dict) {
+        vector<vector<string>> res;
+        queue<string> q;
+        unordered_map<string, Node *> nodes;
+        bool found = false;
+        
+        nodes[start] = NULL;
+        q.push(start);
+        dict.insert(end);// !!!!
+        
+        while(!q.empty()) {
+            
+            unordered_map<string, Node *> nodes_old = nodes;
+            queue<string> q2;
+            while(!q.empty()) {
+                string word = q.front();
+                q.pop();
+                
+                unordered_set<string> inserted;
+                for(int i = 0; i < word.size(); ++i) {
+                    string tmp = word;
+                    for(int c = 'a'; c <= 'z'; ++c) {
+                        tmp[i] = c;
+                        if(dict.find(tmp) == dict.end()) continue; // if not in dict
+                        if(nodes_old.find(tmp) != nodes_old.end()) continue; // if already expanded in prev levels
+
+                        Node *wordNode;
+                        if(nodes.find(tmp) == nodes.end()) {
+                            wordNode = new Node(tmp);
+                            nodes[tmp] = wordNode;
+                        } else {
+                            wordNode = nodes[tmp];
+                        }
+                        
+                        wordNode->prevs.push_back(word);
+                        
+                        if(tmp == end) {
+                            found = true;
+                        } else {
+                            if(inserted.find(tmp) == inserted.end()) {
+                                inserted.insert(tmp);
+                                q2.push(tmp);
+                            }
+                        }
+                    }
+                }
+            }
+            if(found) {
+                vector<string> v;
+                v.push_back(end);
+                backtrackPath(nodes, end, v, res);
+                
+                return res;
+            }
+            q = q2;
+        }
+        
+        return res;
+    }
+    
+    void backtrackPath(unordered_map<string, Node*> &nodes, string str, vector<string> &v, vector<vector<string> > &res) {
+        if(nodes[str] == NULL) {
+            vector<string> v_cpy(v);
+            reverse(v_cpy.begin(), v_cpy.end());
+            res.push_back(v_cpy);
+            return;
+        }
+        
+        Node *node = nodes[str];
+        
+        for(int i = 0; i < node->prevs.size(); ++i) {
+            string prev = node->prevs[i];
+            v.push_back(prev);
+            backtrackPath(nodes, prev, v, res);
+            v.pop_back();
+        }
+    }
+};
+*/
+/*
+// Memory Limit excceed
 struct Node {
     string str;
     vector<Node *> prevs;
