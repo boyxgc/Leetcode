1c8e023047817d5b25d9cf2dcf90ace19789737a
boyxgc <boyxgc@gmail.com>
2014-10-01 10:16:28 -0700

    update

--- First Missing Positive.h
+++ First Missing Positive.h
@@ -1,15 +1,24 @@
 class Solution {
 public:
     int firstMissingPositive(int A[], int n) {
-        unordered_set<int> hash;
-        for(int i = 0; i < n; ++i){
-            hash.insert(A[i]);
-        }
-        for(int i = 1; i <= n+1;++i){
-            if(hash.find(i) == hash.end()){
-                return i;
+        for(int i = 0; i < n; ++i) {
+            if( A[i]-1 < 0 || A[i]-1 >= n) continue;
+            if(A[i] != A[A[i]-1]) {//case [1,1], if no such condition, it will loop forever
+                swap(A, i, A[i]-1);
+                i--;
             }
         }
-        return 1;
+        
+        for(int i = 0; i < n; ++i) {
+            if(i+1 != A[i]) return i+1;
+        }
+        
+        return n+1;
+    }
+    
+    void swap(int A[], int i, int j) {
+        int tmp = A[i];
+        A[i]  = A[j];
+        A[j] = tmp;
     }
 };
