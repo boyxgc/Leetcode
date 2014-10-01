a541c6c585369ee0c8b7c292f3dc410cb96d39c1
boyxgc <boyxgc@gmail.com>
2014-09-30 23:31:23 -0700

    update

--- Sort Colors.h
+++ Sort Colors.h
@@ -1,27 +1,22 @@
 class Solution {
 public:
     void sortColors(int A[], int n) {
-        //one pass solution
-        //meet 0 put from front, meet 2 put from end
+        int front = 0;
+        int back = n - 1;
         
         int i = 0;
-        int front = 0;
-        int back = n-1;
-        while(i <= back){
+        while(i <= back) {
             if(A[i] == 0) {
-                if(i > front) {
-                    A[i] = A[front];   
-                    A[front++] = 0;
-                } else{
-                    front++;
-                    i++;
-                }
-            }else if(A[i] == 2){
+                A[i++] = A[front];
+                A[front++] = 0;
+            } else if (A[i] == 2) {
                 A[i] = A[back];
                 A[back--] = 2;
-            }else{
+            } else {
                 i++;
             }
         }
+        
+        return;
     }
 };
