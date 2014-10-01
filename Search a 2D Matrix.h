fc8fc8549797b59a7f503faa4a87c160e82b8509
boyxgc <boyxgc@gmail.com>
2014-09-30 23:35:09 -0700

    update

--- Search a 2D Matrix.h
+++ Search a 2D Matrix.h
@@ -1,35 +1,42 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int> > &matrix, int target) {
-        if(matrix.size() <= 0)
-            return false;
-        vector<int> col0;
-        for(int i = 0; i < matrix.size(); ++i)
-            col0.push_back(matrix[i][0]);
-        int row = bi_search(col0, 0, col0.size()-1, target);
-        if(col0[row] == target)
-            return true;
-        if(col0[row] > target)
-            row -= 1;
-        if(row < 0)
-            return false;
-        int col = bi_search(matrix[row], 0, matrix[row].size()-1, target);
+        int m = matrix.size();
+        int n = matrix[0].size();
         
-        return matrix[row][col] == target;
-    }
-    
-    int bi_search(vector<int> &v, int l, int h, int target){
-        int m = -1;
+        int l, h, mid;
+        
+        l = 0;
+        h = m - 1;
         while(l <= h) {
-            m = (l+h)/2;
-            if(target < v[m])
-                h = m-1;
-            else if(target == v[m]){
-                break;
+            mid = (l+h)/2;
+            int num = matrix[mid][0];
+            if(num == target) {
+                return true;
+            } else if (num > target) {
+                h = mid - 1;
             } else {
-                l = m+1;
+                l = mid + 1;
             }
         }
-        return m;
+        
+        int row = h;
+        if(row < 0) return false;
+        
+        l = 0;
+        h = n - 1;
+        while(l <= h) {
+            mid = (l+h)/2;
+            int num = matrix[row][mid];
+            if(num == target) {
+                return true;
+            } else if (num > target) {
+                h = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+    
+        return false;
     }
 };
