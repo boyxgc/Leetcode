2993b64d81ea2868f93cdbd533936da98b1d4c47
boyxgc <boyxgc@gmail.com>
2014-10-18 14:43:03 -0700

    [Update]update

--- Convert Sorted Array to Binary Search Tree.h
+++ Convert Sorted Array to Binary Search Tree.h
@@ -10,22 +10,20 @@
 class Solution {
 public:
     TreeNode *sortedArrayToBST(vector<int> &num) {
-        if(num.size() == 0)
-            return NULL;
-        if(num.size() == 1)
-            return new TreeNode(num[0]);
-        if(num.size() == 2){
-            TreeNode * root = new TreeNode(num[1]);
-            root->left = new TreeNode(num[0]);
-            return root;
-        }
-        TreeNode *root = new TreeNode(num[num.size()/2]);
-
-        vector<int> num2(num.begin()+num.size()/2+1, num.end());
-        num.erase(num.begin()+num.size()/2, num.end());// erase second half, remain the first half
         
-        root->left = sortedArrayToBST(num);
-        root->right = sortedArrayToBST(num2);
+        return convert(num, 0, num.size()-1);
+    }
+    
+private:
+    TreeNode *convert(vector<int> &num, int l, int r) {
+        if(l > r) {
+            return NULL;
+        }
+        
+        int mid = (l+r)/2;
+        TreeNode *root = new TreeNode(num[mid]);
+        root->left = convert(num, l, mid - 1);
+        root->right = convert(num, mid+1, r);
         
         return root;
     }
